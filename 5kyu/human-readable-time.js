fcb4e95a03adc7a2a52f52a33b51b7c7a8d1b6b2
Ayula Boyilo <drugifejk@gmail.com>
2022-10-02 18:13:01 +0200

    October 2nd

---
+++ 5kyu/human-readable-time.js
@@ -0,0 +1,26 @@
+// DESCRIPTION:
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+function humanReadable(seconds) {
+  return (
+    ("00" + parseInt(seconds / 3600)).slice(-2) +
+    ":" +
+    ("00" + parseInt((seconds % 3600) / 60)).slice(-2) +
+    ":" +
+    ("00" + parseInt((seconds % 3600) % 60)).slice(-2)
+  );
+}
+
+// function humanReadable(seconds) {
+//     var pad = function(x) { return (x < 10) ? "0"+x : x; }
+//     return pad(parseInt(seconds / (60*60))) + ":" +
+//            pad(parseInt(seconds / 60 % 60)) + ":" +
+//            pad(seconds % 60)
+//   }
